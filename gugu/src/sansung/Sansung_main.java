fd3a1f12e9c1842a5af98dc8c73e8a746dd7add9
SeungWooLee16 <38197683+SeungWooLee16@users.noreply.github.com>
2018-06-09 17:12:37 +0900

    Update Sansung_main.java

--- gugu/src/sansung/Sansung_main.java
+++ gugu/src/sansung/Sansung_main.java
@@ -17,5 +17,12 @@ public class Sansung_main {
 		Sansung_game game = new Sansung_game(nick);// 닉네임 가지고 게임화면 띄우기 
 		
 	}
+	
+	public void showShort(Sansung_nick gui, String nick)
+	{
+		
+		shortType Type = new shortType(nick);//닉네임 가지고 짧은 글 실행
+	}
+	
 
 }
